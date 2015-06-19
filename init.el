2118e64d1d718ddd1926c9b2f455b5f8a8b6a9cb
Alex Savin <karismafilms@gmail.com>
2015-06-19 12:38:26 +0100

    Bringing init.el up to date

--- init.el
+++ init.el
@@ -11,6 +11,9 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(add-to-list 'package-archives 
+             '("org" . "http://orgmode.org/elpa/") t)
+
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
 ;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
@@ -131,9 +134,74 @@
 (load "setup-clojure.el")
 (load "setup-js.el")
 
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2))
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "1a85b8ade3d7cf76897b338ff3b20409cb5a5fbed4e45c6f38c98eee7b025ad4" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" default)))
+ '(org-agenda-files (quote ("~/org/test.org")) t))
+
+(require 'popwin)
+(popwin-mode 1)
+
+(push '(direx:direx-mode :position left :width 25 :dedicated t)
+      popwin:special-display-config)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
+
+(global-set-key (kbd "C-x C-e") 'neotree-toggle)
+
+(load "editorconfig")
+
+(load "evernote-mode.el")
+
+(setq make-backup-files nil) 
+
+(setq org-agenda-files (list "~/org/test.org"
+                             "~/org/alex.org"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'hl-sentence)
+(set-face-attribute 'hl-sentence-face nil
+		    :foreground "white")
+(set-face-attribute 'variable-pitch nil
+ 		    :foreground "gray40")
+
+(add-hook 'twittering-mode-hook
+           (lambda ()
+             (mapc (lambda (pair)
+                     (let ((key (car pair))
+                           (func (cdr pair)))
+                       (define-key twittering-mode-map
+                         (read-kbd-macro key) func)))
+                   '(("F" . twittering-favorite)
+                     ("R" . twittering-replies-timeline)
+                     ("U" . twittering-user-timeline)
+                     ("W" . twittering-update-status-interactive)))))
+
+;; Smooth scrolling (well, relatively)
+(setq scroll-conservatively 10000)
+(setq auto-window-vscroll nil)
+
+;; Default color theme
+(load-theme 'tomorrow-night-eighties)
+
+;; Enabling editorconfig
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(load "editorconfig.el")
+
+(windmove-default-keybindings)
